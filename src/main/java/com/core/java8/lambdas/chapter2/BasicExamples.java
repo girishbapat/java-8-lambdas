b45dc7f6bc9c8b805b4033b40f4eba78d197faf2
girishbapat <girish.bapat@gmail.com>
2014-10-29 20:28:48 +0530

    commit for project structure creation

---
+++ src/main/java/com/core/java8/lambdas/chapter2/BasicExamples.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.core.java8.lambdas.chapter2;
+
+/**
+ * @author Girish Bapat
+ *
+ */
+public class BasicExamples {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
